5ce8b5b973ccc7a43aea69ab01e87d9b575adbac
Brian Waweru <brianwaweru627@gmail.com>
2024-11-08 22:04:33 +0300

    Added phone_number field to customers table schema

--- database/migrations/2024_11_08_185959_create_customers_table.php
+++ database/migrations/2024_11_08_185959_create_customers_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
+            $table->string('phone_number', 20);
             $table->timestamps();
         });
     }
