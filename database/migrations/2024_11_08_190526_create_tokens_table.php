10f99f48318da4c0e8cac96ec9210bd0312b5276
Brian Waweru <brianwaweru627@gmail.com>
2024-11-08 22:27:11 +0300

    Added required fields to tokens table schema

--- database/migrations/2024_11_08_190526_create_tokens_table.php
+++ database/migrations/2024_11_08_190526_create_tokens_table.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\Branch;
+use App\Models\Counter;
+use App\Models\Customer;
+use App\Models\Operator;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +17,12 @@ return new class extends Migration
     {
         Schema::create('tokens', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Customer::class);
+            $table->string('service_type', 64);
+            $table->string('status', 64);
+            $table->foreignIdFor(Branch::class);
+            $table->foreignIdFor(Counter::class);
+            $table->foreignIdFor(Operator::class);
             $table->timestamps();
         });
     }
