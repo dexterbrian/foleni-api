a836f7c5ee3b448b9c946b813bd8f9ddd02aed33
Brian Waweru <brianwaweru627@gmail.com>
2024-11-08 23:04:38 +0300

    Added required fields to tokens, branches & operators table schemas

--- database/migrations/2024_11_08_192322_create_counters_table.php
+++ database/migrations/2024_11_08_192322_create_counters_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Branch;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,8 @@ return new class extends Migration
     {
         Schema::create('counters', function (Blueprint $table) {
             $table->id();
+            $table->foreignIdFor(Branch::class);
+            $table->string('service_type', 64);
             $table->timestamps();
         });
     }
